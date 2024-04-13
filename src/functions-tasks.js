b4580d37dc81d68d40e804fb88316f75831e57c1
D4n1el13and37 <daniel837@mail.ru>
2024-04-13 17:02:24 +0300

    feat: solve retry function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -142,8 +142,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let timesToRun = attempts;
+  return () => {
+    while (timesToRun > 1) {
+      try {
+        return func();
+      } catch (e) {
+        timesToRun -= 1;
+      }
+    }
+    return func();
+  };
 }
 
 /**
